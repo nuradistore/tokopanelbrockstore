4f93576051e0dd8c6ef36d6d414b254408e617d5
nuradistore <130492351+nuradistore@users.noreply.github.com>
2026-03-04 22:45:48 +0700

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import type React from "react"
+import type { Metadata } from "next"
+import { Poppins } from "next/font/google"
+import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/toaster"
+import { appConfig } from "@/data/config"
+import { BackgroundDecor } from "@/components/background-decor"
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-poppins",
+})
+
+export const metadata: Metadata = {
+  title: `${appConfig.nameHost} - Pterodactyl Panel`,
+  description: "Beli panel Pterodactyl dengan mudah dan cepat",
+  icons: {
+    icon: "/itzky.png"
+  }
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning className="dark">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+      </head>
+      <body className={poppins.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+          <BackgroundDecor />
+          {children}
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
